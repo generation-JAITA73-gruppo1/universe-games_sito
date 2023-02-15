a93998cfdee7cf4d6b1970d7340166673aebb2f4
SaxxyLemon46 <119659728+SaxxyLemon46@users.noreply.github.com>
2023-02-15 15:09:58 +0100

    feat: formatting e almost done list e dettaglio

--- src/app/m-recensioni/dettaglio-recensioni/dettaglio-recensioni.component.ts
+++ src/app/m-recensioni/dettaglio-recensioni/dettaglio-recensioni.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable, switchMap } from 'rxjs';
+import { VideogiochiService } from 'src/app/m-videogiochi/service/videogiochi.service';
+import { Recensione } from 'src/app/model/recensione';
+import { Videogioco } from 'src/app/model/videogioco';
+import { RecensioniService } from '../service/recensioni.service';
 
 @Component({
   selector: 'app-dettaglio-recensioni',
   templateUrl: './dettaglio-recensioni.component.html',
-  styleUrls: ['./dettaglio-recensioni.component.css']
+  styleUrls: ['./dettaglio-recensioni.component.css'],
 })
 export class DettaglioRecensioniComponent {
+  reviewDettaglio?: Recensione;
+  reviews$!: Observable<Recensione[]>;
+  relatedGame?: Videogioco;
 
+  constructor(
+    private recensioniwService: RecensioniService,
+    private videogiochiService: VideogiochiService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.reviews$ = this.recensioniwService.getRecensioni();
+
+    this.route.params
+      .pipe(
+        map((params) => params['id'] as string),
+
+        switchMap((id) => this.recensioniwService.getRecensione(id))
+      )
+      .subscribe((recensione) => {
+        this.reviewDettaglio = recensione;
+        if (this.reviewDettaglio !== undefined) {
+          this.videogiochiService
+            .getVideogioco(this.reviewDettaglio.reviewedGame.id)
+            .subscribe((videogioco) => {
+              this.relatedGame = videogioco;
+            });
+        }
+      });
+  }
 }
--- src/app/m-videogiochi/dettaglio-videogioco/dettaglio-videogioco.component.ts
+++ src/app/m-videogiochi/dettaglio-videogioco/dettaglio-videogioco.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Observable, switchMap } from 'rxjs';
+import { Videogioco } from 'src/app/model/videogioco';
+import { VideogiochiService } from '../service/videogiochi.service';
 
 @Component({
   selector: 'app-dettaglio-videogioco',
   templateUrl: './dettaglio-videogioco.component.html',
-  styleUrls: ['./dettaglio-videogioco.component.css']
+  styleUrls: ['./dettaglio-videogioco.component.css'],
 })
 export class DettaglioVideogiocoComponent {
+  gamesDettaglio?: Videogioco;
+  games$!: Observable<Videogioco[]>;
 
+  constructor(
+    private newsService: VideogiochiService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.games$ = this.newsService.getVideogiochi();
+
+    this.route.params
+      .pipe(
+        map((params) => params['id'] as string),
+        // switchMap è un operatore che serve a "cambiare l'observable con un altro"
+        // viene utilizzato nei casi in cui voglio trasmettere dalla pipe un valore
+        // che a sua volta arriva da un altro tubo
+        switchMap((id) => this.newsService.getVideogioco(id))
+      )
+      .subscribe((gioco) => {
+        this.gamesDettaglio = gioco;
+      });
+  }
 }
